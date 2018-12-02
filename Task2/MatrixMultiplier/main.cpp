27185b8d05fa0d7d3960f31beb4e8bab300c312c
gazizullin <gazizullin.malik2010@yandex.ru>
2018-12-02 22:43:00 +0300

    Written

--- Task2/MatrixMultiplier/main.cpp
+++ Task2/MatrixMultiplier/main.cpp
@@ -4,6 +4,10 @@
 
 void read_Matrix(const char *fileName, double ***matrix, size_t& i, size_t& i1);
 
+void write_matrix(const char *filename, double **Matrix, size_t i, size_t i1);
+
+void delete_matrix(double **Matrix, size_t n);
+
 int main(int argc, char **argv) {
 
     size_t nA, kA;
@@ -15,7 +19,7 @@ int main(int argc, char **argv) {
     size_t nB, kB;
     double **MatrixB;
 
-    read_Matrix(argv[1], &MatrixB, nB, kB);
+    read_Matrix(argv[2], &MatrixB, nB, kB);
 
     double **MatrixC = new double*[nA];
 
@@ -36,6 +40,38 @@ int main(int argc, char **argv) {
             MatrixC[i][j] = c_ij;
         }
     }
+
+    write_matrix(argv[3], MatrixC, nA, kB);
+
+    delete_matrix(MatrixA, nA);
+    delete[] MatrixA;
+
+    delete_matrix(MatrixB, nB);
+    delete[] MatrixB;
+
+    delete_matrix(MatrixB, nA);
+    delete[] MatrixC;
+
+}
+
+void delete_matrix(double **Matrix, size_t n) {
+    for (size_t i = 0; i < n; i++) {
+        delete[] Matrix[i];
+    }
+}
+
+void write_matrix(const char *filename, double **Matrix, size_t n, size_t k) {
+
+    std::ofstream file;
+    file.open(filename);
+    file << n << " " << k << "\n";
+    for (size_t i = 0; i < n; i++) {
+        for (size_t j = 0; j < k; j++) {
+            file << Matrix[i][j] << " ";
+        }
+        file << std::endl;
+    }
+    file.close();
 }
 
 void read_Matrix(const char *fileName, double ***matrix, size_t &n, size_t &k) {
